d10b1869a1bbe47917e87a50d9cc0442c8ada1fc
Bailey-1 <44928829+Bailey-1@users.noreply.github.com>
2020-02-08 18:10:31 +0000

    Created the individual profile pages and their API

--- public/index.js
+++ public/index.js
@@ -4,6 +4,22 @@ function removeContentFrom(what) {
 	}
 }
 
+function getAgeFromDate(date) {
+	//date is mm/dd/yyyy
+
+	//get todays date to compare the date with.
+	let today = new Date();
+	console.log('Date', date);
+	let birthDate = new Date(date);
+	console.log('Birthdate', birthDate);
+	let age = today.getFullYear() - birthDate.getFullYear();
+	let m = today.getMonth() - birthDate.getMonth();
+	if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+		age = age - 1;
+	}
+	return age;
+}
+
 async function discoverPage() {
 	const template = document.querySelector('#profile-filters');
 	const clone = document.importNode(template.content, true);
@@ -14,7 +30,14 @@ async function discoverPage() {
 
 async function getProfiles() {
 	const request = await fetch('./api/get/discover');
-	if (!request.ok) console.warn('Could not get posts');
+	if (!request.ok) return console.warn('Could not get /api/get/discover');
+	const profileObj = await request.json();
+	return profileObj;
+}
+
+async function getProfileById(id) {
+	const request = await fetch(`./api/get/profile/${id}`);
+	if (!request.ok) return console.warn(`Could not get /api/get/profile/${id}`);
 	const profileObj = await request.json();
 	return profileObj;
 }
@@ -25,19 +48,41 @@ function createProfileElement(profile) {
 	const clone = document.importNode(template.content, true);
 	//console.log('This the clone', clone);
 	clone.querySelector('.dogProfile').id = profile.id;
-	clone.querySelector('#name').textContent = profile.name;
-	clone.querySelector('#profile-location').textContent = profile.location;
-	clone.querySelector('#profile-owner').textContent = profile.owner;
+	clone.querySelector('#name').textContent = `${profile.name.substring(
+		0,
+		12
+	)}, ${getAgeFromDate(profile.birthday)}`;
+	clone.querySelector(
+		'#profile-location'
+	).textContent = `Location - ${profile.location.substring(0, 10)}`;
+	clone.querySelector(
+		'#profile-owner'
+	).textContent = `Owner - ${profile.owner.substring(0, 10)}`;
 	document.querySelector('#discover-profiles').appendChild(clone);
 }
 
-function createProfilePage() {
+async function createProfilePage() {
 	console.log('Name is clicked');
-	const parentElement = event.srcElement.parentElement.parentElement;
+	const parentElementID = event.srcElement.parentElement.parentElement.id;
+	console.log('Parent Element: ', parentElementID);
 	removeContentFrom(document.querySelector('#main-body-content'));
 
 	const template = document.querySelector('#main-profile-content');
 	const clone = document.importNode(template.content, true);
+
+	const profile = await getProfileById(parentElementID);
+	console.log(profile);
+	clone.querySelector('#profile-page-name').textContent = `${
+		profile.name
+	}, ${getAgeFromDate(profile.birthday)}`;
+	clone.querySelector('#profile-page-about-me').textContent =
+		profile.profileDetails.aboutMe;
+	clone.querySelector('#profile-page-location').textContent = profile.location;
+	clone.querySelector('#profile-page-likes').textContent =
+		profile.profileDetails.likes;
+	clone.querySelector('#profile-page-dislikes').textContent =
+		profile.profileDetails.dislikes;
+	clone.querySelector('#profile-page-birthday').textContent = profile.birthday;
 
 	document.querySelector('#main-body-content').appendChild(clone);
 }
--- todo.md
+++ todo.md
@@ -1,14 +1,25 @@
-# todo
+# TODO
 
 ## Frontend:
 
-1. Break up the main pages into templates.
-2. Improve the overall design and decide if I want to use "pages" or "Items" on the page to display a profile.
+1. ~~Break up the main pages into templates.~~
+2. ~~Improve the overall design and decide if I want to use "pages" or "Items" on the page to display a profile.~~
 3. Add a multi-filter to make it easy to find specific types of options and filter multiple options at the same time such as location and breed and age.
+4. Allow the user to create a "profile" for their dog on the front end and develop the APIs needed to save and store the data.
 
-## API:
+---
 
-> use :account and :profile to see if the user should have access to those resources and deny them if the profile is not linked to the account.
+## Maybe if I have enough time:
+
+- Sort out the date format to **DD/MM/YYYY** both for input and output.
+- Improve CSS and add slight animations on items.
+- Improve the design and colours scheme.
+
+---
+
+## API Overview Plan:
+
+> use :account and :profile to see if the logged in user should have access to those resources and deny them if the profile is not linked to the account. Security?
 
 > Need to deal with real time messaging.
 
@@ -17,19 +28,19 @@
 1. GET General server information - might need this - MOTD?
 
 ```
-/api/general
+/api/get/general
 ```
 
 2. GET potential matches with discover for profile id
 
 ```
-/api/discover/:id
+/api/get/discover/:id
 ```
 
 2. GET Request JSON object for specific profile
 
 ```
-/api/:account/:profile/:id
+/api/get/:account/:profile/:id
 ```
 
 ### **POST Requests**
@@ -37,17 +48,17 @@
 1. POST Add new profile
 
 ```
-/api/:accountID/new
+/api/post/:accountID/new
 ```
 
 2. POST Changes to a profile
 
 ```
-/api/:accountID/change/:profileID
+/api/post/:accountID/change/:profileID
 ```
 
 3. POST Add image to account
 
 ```
-/api/:accountID/:profileID/image
+/api/post/:accountID/:profileID/image
 ```
